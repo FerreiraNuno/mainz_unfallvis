0337618c167ca35199e671120a7575bef501ee8d
Yannik <64977942+Ynnik@users.noreply.github.com>
2024-12-14 00:29:00 +0100

    Map Changes, Config for Rewriting Attributes and Dash-Bootstrap Components

---
+++ config.py
@@ -0,0 +1,91 @@
+import consts as CONSTS
+import plotly.express as px
+
+rewrite_unfallk_wahr = {
+    "0": "Unfall mit Getöteten",
+    "1": "Unfall mit Schwerverletzten",
+    "2": "Unfall mit Leichtverletzten",
+}
+rewrite_unfall_art = {
+    "0": "Unfall anderer Art",
+    "1": "Zusammenstoß mit anfahrendem/anhaltendem/ruhendem Fahrzeug",
+    "2": "Zusammenstoß mit vorausfahrendem/wartendem Fahrzeug",
+    "3": "Zusammenstoß mit seitlich in gleicher Richtung fahrendem Fahrzeug",
+    "4": "Zusammenstoß mit entgegenkommendem Fahrzeug",
+    "5": "Zusammenstoß mit einbiegendem/kreuzendem Fahrzeug",
+    "6": "Zusammenstoß zwischen Fahrzeug und Fußgänger",
+    "7": "Aufprall auf Fahrbahnhindernis",
+    "8": "Abkommen von Fahrbahn nach rechts",
+    "9": "Abkommen von Fahrbahn nach links",
+}
+rewrite_unfall_typ = {
+    "1": "Fahrunfall",
+    "2": "Abbiegeunfall",
+    "3": "Einbiegen / Kreuzen-Unfall",
+    "4": "Überschreiten-Unfall",
+    "5": "Unfall durch ruhenden Verkehr",
+    "6": "Unfall im Längsverkehr",
+    "7": "sonstiger Unfall",
+}
+rewrite_licht = {"0": "Tageslicht", "1": "Dämmerung", "2": "Dunkelheit"}
+rewrite_strassen_ver = {
+    "0": "trocken",
+    "1": "nass/feucht/schlüpfrig",
+    "2": "winterglatt",
+}
+rewrite_tag_kategorie = {"0": "Wochenende", "1": "Wochentag"}
+rewrite_strassen_typ = {
+    "construction": "Bauarbeiten (construction site)",
+    "living_street": "Verkehrsberuhigter Bereich",
+    "motorway": "Autobahn",
+    "motorway_link": "Autobahnanschluss (motorway link)",
+    "path": "Weg (or Pfad for footpaths)",
+    "primary": "Hauptstraße (primary road)",
+    "primary_link": "Hauptstraßenanschluss (primary road link)",
+    "residential": "Wohnstraße",
+    "secondary": "Nebenstraße (secondary road)",
+    "secondary_link": "Nebenstraßenanschluss (secondary road link)",
+    "service": "Serviceweg (or Zubringerstraße for service streets)",
+    "tertiary": "Tertiärstraße (tertiary road)",
+    "track": "Fahrweg (or Wirtschaftsweg for agricultural/forestry tracks)",
+    "trunk": "Schnellstraße (or Hauptverkehrsstraße)",
+    "trunk_link": "Schnellstraßenanschluss (trunk road link)",
+    "unclassified": "Nicht klassifiziert (unclassified)",
+}
+
+
+rewriteDict = {
+    CONSTS.UNFALLART: rewrite_unfall_art,
+    CONSTS.UNFALLTYP: rewrite_unfall_typ,
+    CONSTS.UNFALLKLASSE_WAHR: rewrite_unfallk_wahr,
+    CONSTS.LICHTVERHAELTNISSE: rewrite_licht,
+    CONSTS.STRASSENVERHAELTNISSE: rewrite_strassen_ver,
+    CONSTS.STRASSENART: rewrite_strassen_typ,
+    CONSTS.TAGKATEGORIE: rewrite_tag_kategorie,
+}
+
+
+customColoringMap = {
+    CONSTS.UNFALLART: px.colors.sequential.Aggrnyl,
+    CONSTS.UNFALLTYP: px.colors.sequential.Aggrnyl,
+    CONSTS.UNFALLKLASSE_WAHR: [
+        "rgb(255, 0, 0)",
+        "rgb(0, 255, 0)",
+        "rgb(0, 0, 255)",
+    ],
+    CONSTS.LICHTVERHAELTNISSE: px.colors.sequential.Aggrnyl,
+    CONSTS.STRASSENVERHAELTNISSE: px.colors.sequential.Aggrnyl,
+    CONSTS.STRASSENART: px.colors.sequential.Aggrnyl,
+    CONSTS.TAGKATEGORIE: px.colors.sequential.Aggrnyl,
+}
+
+
+highlighting_dropdown = [
+    CONSTS.UNFALLKLASSE_WAHR,
+    CONSTS.UNFALLART,
+    CONSTS.UNFALLTYP,
+    CONSTS.LICHTVERHAELTNISSE,
+    CONSTS.STRASSENVERHAELTNISSE,
+    CONSTS.STRASSENART,
+    CONSTS.TAGKATEGORIE,
+]
--- main.py
+++ main.py
@@ -3,121 +3,283 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import consts as CONSTS
+from config import *
 from datetime import datetime
+
+import dash_bootstrap_components as dbc
 
 # Create Dash app
 app = Dash(__name__)
+
+
 # Load data
-accidents_data = pd.read_csv("./data/Verkehrsunfalldaten.csv", delimiter=';', decimal=',')
-# Prepare data
-accidents_data[CONSTS.UNFALLKLASSE_WAHR] = accidents_data[CONSTS.UNFALLKLASSE_WAHR].astype(str)
-
-accidents_data[CONSTS.CUSTOM_DATETIME] = pd.to_datetime({'year': accidents_data[CONSTS.JAHR], 'month': accidents_data[CONSTS.MONAT], 'day': 1}) #prepare datetime column
+accidents_data = pd.read_csv("./data/Verkehrsunfalldaten.csv", delimiter=";", decimal=",")
 
 
-def prepare_map(attr_to_color_by, coloring_map, data):
+def init():
+    # Prepare data
+    accidents_data[CONSTS.CUSTOM_DATETIME] = pd.to_datetime(
+        {
+            "year": accidents_data[CONSTS.JAHR],
+            "month": accidents_data[CONSTS.MONAT],
+            "day": 1,
+        }
+    )  # prepare datetime column
+    accidents_data[CONSTS.UNFALLKLASSE_WAHR] = accidents_data[
+        CONSTS.UNFALLKLASSE_WAHR
+    ].astype(str)
+    accidents_data[CONSTS.UNFALLART] = accidents_data[CONSTS.UNFALLART].astype(str)
+    accidents_data[CONSTS.UNFALLTYP] = accidents_data[CONSTS.UNFALLTYP].astype(str)
+    accidents_data[CONSTS.LICHTVERHAELTNISSE] = accidents_data[
+        CONSTS.LICHTVERHAELTNISSE
+    ].astype(str)
+    accidents_data[CONSTS.STRASSENVERHAELTNISSE] = accidents_data[
+        CONSTS.STRASSENVERHAELTNISSE
+    ].astype(str)
+    accidents_data[CONSTS.STRASSENART] = accidents_data[CONSTS.STRASSENART].astype(str)
+    accidents_data[CONSTS.TAGKATEGORIE] = accidents_data[CONSTS.TAGKATEGORIE].astype(str)
+
+
+def prepare_map(attr_to_color_by, data):
     # Add scatter map with coordinates of accidents
     map_fig = px.scatter_mapbox(
         data,
         lat=CONSTS.LATITUDE,
         lon=CONSTS.LONGITUDE,
-        hover_name="Stadtteil",
+        hover_name=CONSTS.STADTTEIL,
         hover_data=[
             CONSTS.JAHR,
             CONSTS.UNFALLKLASSE_BESTIMMT,
-            CONSTS.WAHRSCHEINLICHKEIT_KLASSE_0,
-            CONSTS.WAHRSCHEINLICHKEIT_KLASSE_1,
-            CONSTS.WAHRSCHEINLICHKEIT_KLASSE_2,
-            CONSTS.UNSICHERHEITS_SCORE
+            CONSTS.STRASSENVERHAELTNISSE,
+            CONSTS.STRASSENART,
+            CONSTS.UNSICHERHEITS_SCORE,
+            CONSTS.LICHTVERHAELTNISSE,
+            CONSTS.UNFALLTYP,
         ],
         custom_data=[data.index],  # Pass the DataFrame index as custom data
         zoom=12,
         color=attr_to_color_by,
-        color_discrete_map=coloring_map
+        color_discrete_sequence=customColoringMap.get(attr_to_color_by),
     )
-    map_fig.update_layout(mapbox_style="https://tiles-eu.stadiamaps.com/styles/alidade_smooth_dark.json")
-    map_fig.update_layout(autosize=False, width=800, height=800, margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    map_fig.update_layout(showlegend=False)  # Legende ausblenden
+    map_fig.update_layout(
+        mapbox_style="https://tiles-eu.stadiamaps.com/styles/alidade_smooth_dark.json",
+        #autosize=False, width=800, height=800, margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+    )
+    # Rewrite Labels
+    map_fig.for_each_trace(
+        lambda trace: (
+            trace.update(name=rewriteDict.get(attr_to_color_by)[trace.name])
+            if trace.name in rewriteDict.get(attr_to_color_by)
+            else None
+        )
+    )
     return map_fig
 
-@app.callback(
-    Output('map', 'figure'),
-    Input('year_range_slider', 'marks'),
-    Input('year_range_slider', 'value')
-)
-def update_map(marks_dict, values):
-    if None not in (marks_dict, values):
-        marks_dict = dict(marks_dict)
-        start = datetime.strptime(marks_dict.get(str(values[0]))["label"], '%m-%Y') #create datetime objects to compare against
-        end = datetime.strptime(marks_dict.get(str(values[1]))["label"], '%m-%Y') 
-        filtered_data = accidents_data.loc[
-            lambda x: ((x[CONSTS.CUSTOM_DATETIME] >= start) & (x[CONSTS.CUSTOM_DATETIME] <= end))
-        ]
-        return prepare_map(CONSTS.UNFALLKLASSE_WAHR, {"0": "red", "1": "yellow", "2": "green"}, filtered_data)
-    return prepare_map(CONSTS.UNFALLKLASSE_WAHR, {"0": "red", "1": "yellow", "2": "green"}, accidents_data)
 
 @app.callback(
-    Output('bar-chart', 'figure'),
-    Input('map', 'hoverData')
+    Output("map", "figure"),
+    Input("year_range_slider", "marks"),
+    Input("year_range_slider", "value"),
+    Input("highlighting_dropdown", "value"),
 )
-def update_bar_chart(hover_data):
-    if hover_data:
+def update_map(marks_dict, values, highlighting_dropdown):
+    if None not in (marks_dict, values):
+        marks_dict = dict(marks_dict)
+        start = datetime.strptime(
+            marks_dict.get(str(values[0]))["label"], "%m-%Y"
+        )  # create datetime objects to compare against
+        end = datetime.strptime(marks_dict.get(str(values[1]))["label"], "%m-%Y")
+        filtered_data = accidents_data.loc[
+            lambda x: (
+                (x[CONSTS.CUSTOM_DATETIME] >= start)
+                & (x[CONSTS.CUSTOM_DATETIME] <= end)
+            )
+        ]
+        return prepare_map(highlighting_dropdown, filtered_data)
+    return prepare_map(highlighting_dropdown, accidents_data)
+
+
+@app.callback(
+    Output("bar-chart-predicted-accident-class", "figure"), 
+    Input("map", "clickData")
+)
+def update_bar_chart(click_data):
+    if click_data:
         # Extract latitude and longitude from hoverData
-        hovered_lat = hover_data['points'][0]['lat']
-        hovered_lon = hover_data['points'][0]['lon']
+        hovered_lat = click_data["points"][0]["lat"]
+        hovered_lon = click_data["points"][0]["lon"]
 
         # Filter the dataset to find the corresponding row
         point_data = accidents_data.loc[
-            (accidents_data[CONSTS.LATITUDE] == hovered_lat) &
-            (accidents_data[CONSTS.LONGITUDE] == hovered_lon)
+            (accidents_data[CONSTS.LATITUDE] == hovered_lat)
+            & (accidents_data[CONSTS.LONGITUDE] == hovered_lon)
         ]
 
         if not point_data.empty:
             # Prepare data for the bar chart
-            bar_data = pd.DataFrame({
-                "Unfallklasse Vorhersage": ["Tödlicher Ausgang", "Schwerverletzte", "Leichtverletzte"],
-                "Value": [
-                    point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_0].values[0],
-                    point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_1].values[0],
-                    point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_2].values[0]
-                ]
-            })
+            bar_data = pd.DataFrame(
+                {
+                    "Unfallklasse Vorhersage": [
+                        "Tödlicher Ausgang",
+                        "Schwerverletzte",
+                        "Leichtverletzte",
+                    ],
+                    "Value": [
+                        point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_0].values[0],
+                        point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_1].values[0],
+                        point_data[CONSTS.WAHRSCHEINLICHKEIT_KLASSE_2].values[0],
+                    ],
+                }
+            )
 
             # Create bar chart
-            fig = px.bar(bar_data, x="Unfallklasse Vorhersage", y="Value", title="Vorhersage nach Unfallklasse")
-            fig.update_layout(height=800)
+            fig = px.bar(
+                bar_data,
+                x="Unfallklasse Vorhersage",
+                y="Value",
+                title="Vorhersage nach Unfallklasse",
+            )
+            #fig.update_layout(height=800)
             return fig
 
     # Default empty figure
     return go.Figure()
 
+@app.callback(
+    Output("bar-chart-accident-participants", "figure"), 
+    Input("map", "clickData")
+)
+def update_accident_participants_bar_chart(click_data):
+    if click_data:
+        # Extract latitude and longitude from hoverData
+        hovered_lat = click_data["points"][0]["lat"]
+        hovered_lon = click_data["points"][0]["lon"]
 
-def generateMonthAndYearMarks():
-    start = str(accidents_data[CONSTS.JAHR].min()) + '-' + str(accidents_data[CONSTS.MONAT].min()) + '-' + '1'
-    end = str(accidents_data[CONSTS.JAHR].max()) + '-' + str(accidents_data[CONSTS.MONAT].max()) + '-' + '1' 
-    month_year_range = pd.date_range(start=start, end=end, freq='QS') # QS DateOffset Quarter Starting
+        # Filter the dataset to find the corresponding row
+        point_data = accidents_data.loc[
+            (accidents_data[CONSTS.LATITUDE] == hovered_lat)
+            & (accidents_data[CONSTS.LONGITUDE] == hovered_lon)
+        ]
 
-    return {each : {"label": str(date), "style": {"transform": "rotate(45deg)"}} for each, date in enumerate(month_year_range.unique().strftime('%m-%Y'))}
+        if not point_data.empty:
+            # Prepare data for the bar chart
+            bar_data = pd.DataFrame(
+                {
+                    "Beteiligte": ["IstRad", "IstPKW", "IstFuss", "IstKrad", "IstSonstig"],
+                    "Value": [
+                        point_data[CONSTS.ISTFUSS].values[0],
+                        point_data[CONSTS.ISTKRAD].values[0],
+                        point_data[CONSTS.ISTPKW].values[0],
+                        point_data[CONSTS.ISTRAD].values[0],
+                        point_data[CONSTS.ISTSONSTIG].values[0],
+                    ],
+                }
+            )
+
+            # Create bar chart
+            fig = px.bar(
+                bar_data,
+                x="Beteiligte",
+                y="Value",
+                title="Unfallbeteiligte",
+            )
+            #fig.update_layout(height=800)
+            fig.update_traces(marker_color='blue', marker_line_color='blue', marker_line_width = 12)
+            return fig
+
+    # Default empty figure
+    return go.Figure()
+
+def generateMonthAndYearMarks(df):
+    start = str(df[CONSTS.JAHR].min()) + "-" + str(df[CONSTS.MONAT].min()) + "-" + "1"
+    end = str(df[CONSTS.JAHR].max()) + "-" + str(df[CONSTS.MONAT].max()) + "-" + "1"
+    month_year_range = pd.date_range(
+        start=start, end=end, freq="QS"
+    )  # QS DateOffset Quarter Starting
+
+    return {
+        each: {"label": str(date), "style": {"transform": "rotate(45deg)"}}
+        for each, date in enumerate(month_year_range.unique().strftime("%m-%Y"))
+    }
+
 
 def main():
-  marksMonthYear = generateMonthAndYearMarks()
-  # Set Dash layout for displaying the map and the bar chart
-  app.layout = html.Div([
-      html.H4('Unfall-Dashboard'),
-      html.Div([
-        dcc.Graph(id="map",figure=prepare_map(CONSTS.UNFALLKLASSE_WAHR, {"0": "red", "1": "yellow", "2": "green"}, accidents_data), config={'scrollZoom': True}),
-        dcc.Graph(id="bar-chart")
-      ], style={'display': 'flex'}),
-      dcc.RangeSlider(
-          id="year_range_slider",
-          min=0,
-          max=len(marksMonthYear)-1,
-          step=1,
-          marks=marksMonthYear
-      )
-  ])
-  
-  app.run_server(debug=True, port=8081)
+    marksMonthYear = generateMonthAndYearMarks(accidents_data)
+
+    # Set Dash layout for displaying the map and the bar chart
+    app.layout = html.Div(
+        [
+            html.H4("Unfall-Dashboard"),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            html.Div(
+                                dcc.Dropdown(
+                                    highlighting_dropdown,
+                                    id="highlighting_dropdown",
+                                    searchable=False,
+                                    value=highlighting_dropdown[0],
+                                )
+                            ),
+                            html.Div(
+                                dcc.Graph(
+                                    id="map",
+                                    figure=prepare_map(
+                                        CONSTS.UNFALLKLASSE_WAHR, accidents_data
+                                    ),
+                                    config={"scrollZoom": True},
+                                ),
+                                style={
+                                    # TODO
+                                },
+                            ),
+                            html.Div(
+                                dcc.RangeSlider(
+                                    id="year_range_slider",
+                                    min=0,
+                                    max=len(marksMonthYear) - 1,
+                                    step=1,
+                                    marks=marksMonthYear,
+                                    pushable=1,
+                                )
+                            ),
+                        ],
+                        style={
+                            # TODO
+                        },
+                    ),
+                    dbc.Col(
+                        [
+                            dbc.Row(
+                                dbc.Col(
+                                    html.Div(
+                                        dcc.Graph(
+                                            id="bar-chart-predicted-accident-class"
+                                        )
+                                    ),
+                                )
+                            ),
+                            dbc.Row(
+                                dbc.Col(
+                                    html.Div(
+                                        dcc.Graph(id="bar-chart-accident-participants"),
+                                    ),
+                                )
+                            ),
+                        ],
+                        style={},  # todo
+                    ),
+                ],
+                style={"align-items": "end"},
+            ),
+        ]
+    )
+
+    app.run_server(debug=True, port=8081)
+
 
 if __name__ == "__main__":
+    init()
     main()
